e22c531fa6ac576fe07a4fc226dffa4413e128fe
Highoc <door0172@gmail.com>
2017-05-07 20:42:18 +0300

    Добавлено : - Ничего нового Завтра начну писать Engine

--- Game/Game/Includes/Object.h
+++ Game/Game/Includes/Object.h
@@ -4,22 +4,24 @@
 #include "SFML//Main.hpp"
 #include "SFML//Graphics.hpp"
 
-extern class Engine {};
+//extern class Engine {};
 
 class Object 
 {
 public:
-	Object();
-	Object(Vector coordinate, Vector velocity, size_t weight, sf::Sprite sprite);
+	Object(Vector coordinate, Vector velocity, size_t weight, /*sf::Sprite sprite,*/ size_t level);
+
+	void Dump();
+	void Dump(FILE *file);
 
 private:
-	virtual void Control();
+	/*virtual void Control();
 	virtual void Logic();
 	virtual void Physic();
-	virtual void Draw();
+	virtual void Draw();*/
 	
-	Engine *engine;
-	sf::Sprite sprite_;
+//	Engine *engine_;
+//	sf::Sprite sprite_;
 	size_t level_;
 
 	Vector coordinate_;
@@ -27,3 +29,39 @@ private:
 
 	size_t weight_;
 };
+
+Object::Object(Vector coordinate, Vector velocity, size_t weight, /*sf::Sprite sprite,*/ size_t level) :
+//	engine_(NULL),
+//	sprite_(sprite),
+	level_(level),
+	coordinate_(coordinate),
+	velocity_(velocity),
+	weight_(weight)
+{
+	//Не забыть про Engine
+}
+
+void Object::Dump()
+{
+	printf("Object {\n");
+//	printf("\tengine     : 0x%p;\n", engine_);
+//	printf("\tsprite     : 0x%p;\n", &sprite_);
+	printf("\tlevel      : %zu;\n", level_);
+	printf("\tcoordinate : "); coordinate_.Dump();
+	printf("\tvelocity   : "); velocity_.Dump();
+	printf("\tweight     : %zu\n", weight_);
+	printf("}\n");
+}
+
+void Object::Dump(FILE *file)
+{
+	fprintf(file, "Object {\n");
+//	fprintf(file, "\tengine     : 0x%p;\n", engine_);
+//	fprintf(file, "\tsprite     : 0x%p;\n", &sprite_);
+	fprintf(file, "\tlevel      : %zu;\n", level_);
+	fprintf(file, "\tcoordinate : "); coordinate_.Dump();
+	fprintf(file, "\tvelocity   : "); velocity_.Dump();
+	fprintf(file, "\tweight     : %zu\n", weight_);
+	fprintf(file, "}\n");
+
+}
--- Game/Game/Tests/Test_Object.cpp
+++ Game/Game/Tests/Test_Object.cpp
@@ -5,7 +5,12 @@
 int main()
 {
 	printf("+");
+	Vector coord(1, 2);
+	Vector veloc(3, 4);
+	//sf::Sprite sprite;
 
+	Object test(coord, veloc, 10, 2);
+	test.Dump();
 
 	system("pause");
 	return 0;
