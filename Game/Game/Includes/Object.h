ebb4d67a158a81ffd4da80dd7bad94e6fc8d1700
Highoc <door0172@gmail.com>
2017-05-07 18:36:10 +0300

    test

--- Game/Game/Includes/Object.h
+++ Game/Game/Includes/Object.h
@@ -1,1 +1,24 @@
 #pragma once
+
+extern class Vector {};
+extern class Sprite {};
+
+class Object 
+{
+public:
+	Object();
+	Object(Vector coordinate, Vector velocity, size_t weight, Sprite sprite);
+
+private:
+	virtual void Control();
+	virtual void Logic();
+	virtual void Physic();
+	virtual void Draw();
+	
+	Sprite sprite_;
+	
+	Vector coordinate_;
+	Vector velocity_;
+
+	size_t weight_;
+};
